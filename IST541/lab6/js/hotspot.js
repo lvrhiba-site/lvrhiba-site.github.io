fa3dba800bda7d3a7fe6526fd2ec9ffcd7b9dbbe
lvrhiba-site <90274801+lvrhiba-site@users.noreply.github.com>
2021-10-10 13:55:24 -0700

    lab6

---
+++ IST541/lab6/js/hotspot.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+$( function() {
+   $("map area").click( function(){
+
+      if ($(this).attr("id") == "Egypt") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("No, Egypt among the countries that concidered African Arab countries");
+      }
+
+      if ($(this).attr("id") == "Levant") {
+         $("#feedback").css("color","green");
+         $("#feedback").html("Yes, Syria, Lebanon, Jordan and Palestine considered the countries of Al Mashriq which is,the levant");
+      }
+
+      if ($(this).attr("id") == "Sudan") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("No,Sudan speakes the Arabic Language but is not among the Levant because it is not at the Mideterrenian shore ");
+      }
+
+  $("#feedback").css("backgroundColor","yellow");
+  $('#myModal').modal("show");
+
+
+   });
+}); //end main jQuery function
+
+
+
